44862de35e5aeab08905d838deada884d4acd087
Trung Dinh Quang <trungdq88@gmail.com>
2018-02-04 22:01:15 +0700

    Search for stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,7 @@
 class Stock < ApplicationRecord
+  def self.new_from_lookup(ticker)
+    stock = StockQuote::Stock.quote(ticker)
+    price = stock.l.gsub(',', '').to_d
+    new(name: stock.name, ticker: stock.symbol, last_price: price)
+  end
 end
