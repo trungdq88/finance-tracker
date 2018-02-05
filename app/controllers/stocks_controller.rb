d02484bbda9432abf87bf27c64a48451d4cb0195
Trung Dinh Quang <trungdq88@gmail.com>
2018-02-05 08:03:54 +0700

    Search stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,15 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+    else
+      redirect_to my_portfolio_path
+      return
+    end
+
+    if !@stock
+      flash.now[:error] = 'Stock ticker cannot be found'
+    end
     render 'users/my_portfolio'
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,12 @@
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker)
-    stock = StockQuote::Stock.quote(ticker)
-    price = stock.l.gsub(',', '').to_d
-    new(name: stock.name, ticker: stock.symbol, last_price: price)
+    begin
+      stock = StockQuote::Stock.quote(ticker)
+      price = stock.l.gsub(',', '').to_d
+      new(name: stock.name, ticker: stock.symbol, last_price: price)
+    rescue Exception => e
+      logger.debug e
+      nil
+    end
   end
 end
